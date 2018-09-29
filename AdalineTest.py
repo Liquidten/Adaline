f0f38daec4269e63ec8bfd92630ac45974feba9d
Sameep Shah <39490840+Liquidten@users.noreply.github.com>
2018-09-29 00:37:23 -0500

    Add files via upload

--- AdalineTest.py
+++ AdalineTest.py
@@ -9,11 +9,12 @@ Created on Wed Sep 26 21:57:11 2018
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import normalize
+from sklearn import preprocessing
 
 import AdalineSGD
 import Data_Processing
 import FeatureGraphs
+from sklearn.metrics import accuracy_score
 
 
 def build_plot():
@@ -24,52 +25,44 @@ def build_plot():
     plt.show()
 
 
-def get_accuracy(actual, predicted):
-    counter = 0
-    for i in range(actual.shape[0]):
-        if actual[i] == predicted[i]:
-            counter += 1
-
-    accuracy = counter / actual.shape[0] * 100
-    print('The accuracy of the prediction is ' + str(round(accuracy, 2)) + '%')
-
-
-
 dp = Data_Processing.DataProcessing()
-#df = pd.read_csv('train.csv')
+
 PATH = "../Adaline/titanic_train.csv"
     
 #load the csv file into pandas dataframe
 DF = pd.read_csv(PATH)
 
-# Determining the most predictive feature
-FeatureGraphs.Heat_map(DF)
-#FeatureGraphs.graphs(DF)
-
 #Data cleaning
 df_train = dp.data_clean(DF)
+#df_train = dc.clean_data(DF)
+
+# Determining the most predictive feature
+FeatureGraphs.Heat_map(df_train)
     
-X1 = df_train.drop('Survived',axis=1)
-#y = df_train['Survived']
+X1 = df_train.drop('Survived',axis=1).values
 
-X = X1.values[:, 1:]
-X = normalize(X, axis=0, norm='max')
-#print(X.shape)
+min_max_sclaer = preprocessing.MinMaxScaler()
+x_scaled = min_max_sclaer.fit_transform(X1)
+X = x_scaled
+#print(X)
 
-y = df_train.values[:, 0]
-#print(y.shape)
+y = df_train['Survived'].values
 
 # Splitting dataset into 70% train and 30% test
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.3, random_state=0)
+    X, y, test_size=0.3, random_state = 11)
 
 # Training Adaline classifier
-ada = AdalineSGD.AdalineSGD(eta=0.0001, n_iter=30)
+ada = AdalineSGD.AdalineSGD(eta=0.001, n_iter=100)
 ada.fit(X_train, y_train)
 
+
 # Evaluating on test data
-get_accuracy(y_test, ada.predict(X_test))
+#print(ada.predict(X_test))
+acc = accuracy_score(y_test, ada.predict(X_test))
+print ("Test Accuracy :: ", acc*100)
 
 # Building plot for the cost function
 build_plot()
+#task_four()
 
